78cfae6fccd4e8c7e5b6efc47cd669442e728c28
suprabhat kumar <suprabhat2018@gmail.com>
2022-07-31 15:04:56 +0530

    added styles to layouts

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import Head from 'next/head'
 import {AppBar, Typography, Toolbar, Container} from '@material-ui/core'
+import useStyles from '../utils/Layout'
 
 export default function Layout({children}) {
-  return (
-    <div>
-        <Head>
-            <title>Shopping Zone</title>
-        </Head>
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6"> Shopping Zone </Typography>
-            </Toolbar>
-        </AppBar>
-        <Container>
-            {children}
-        </Container>
-        <footer>
-            <Typography variant="body2">
-                All rights reserved. &copy; {new Date().getFullYear()} Shopping Zone
-            </Typography>
-        </footer>
-    </div>
-  )
+    const classes = useStyles();
+
+    return (
+        <div>
+            <Head>
+                <title>Shopping Zone</title>
+            </Head>
+            <AppBar position="static" className={classes.navbar}>
+                <Toolbar>
+                    <Typography variant="h6"> Shopping Zone </Typography>
+                </Toolbar>
+            </AppBar>
+            <Container className={classes.main}>
+                {children}
+            </Container>
+            <footer className={classes.footer}>
+                <Typography variant="body2">
+                    All rights reserved. &copy; {new Date().getFullYear()} Shopping Zone
+                </Typography>
+            </footer>
+        </div>
+    )
 }
